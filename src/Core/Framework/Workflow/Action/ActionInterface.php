d4c90b047f72e9ae332705c47e2009da72315e74
Jan-Erik Spreng <j.spreng@kellerkinder.de>
2019-04-11 12:19:29 +0200

    Add slack action

--- src/Core/Framework/Workflow/Action/ActionInterface.php
+++ src/Core/Framework/Workflow/Action/ActionInterface.php
@@ -6,13 +6,6 @@ use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
 interface ActionInterface
 {
-    public const FIELD_TYPE_TEXT = 'text';
-    public const FIELD_TYPE_TEXTAREA = 'textarea';
-    public const FIELD_TYPES = [
-        self::FIELD_TYPE_TEXT,
-        self::FIELD_TYPE_TEXTAREA,
-    ];
-
     public function getHandlerIdentifier(): string;
 
     public function execute(SalesChannelContext $context, array $configuration): void;
